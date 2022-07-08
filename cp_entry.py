69dcab4da5de929e0dfa3d2f0bf878b8f53d4a39
DragonFighter603 <kuhn.christopher.a@gmail.com>
2022-07-08 17:15:58 +0200

    Update cp_entry.py

--- cp_entry.py
+++ cp_entry.py
@@ -1,6 +1,8 @@
 import shutil
 import os
+import build_launcher
 
 if __name__ == '__main__':
     shutil.copy('entrypoint.py', '../entrypoint.py')
+    build_launcher.build()
     os.system('python ../entrypoint.py')
