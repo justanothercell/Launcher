2df428f3e75deeb66c76e611e16d8c3dafc0fcc3
DragonFighter603 <kuhn.christopher.a@gmail.com>
2022-07-08 17:10:21 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,16 +3,20 @@ import requests
 
 
 def entry():
+    print('Getting origin last commit sha...')
     sha = requests.get('https://api.github.com/repos/DragonFIghter603/Launcher/commits/main').json()['sha']
 
-    if not os.path.exists('sha1'):
-        with open('sha', 'w') as sha_file:
-            sha_file.write('null')
+    try:
+        with open('sha', 'r') as sha_file:
+            read_sha = sha_file.read().strip()
+    except FileNotFoundError:
+        read_sha = 'null'
 
-    with open('sha', 'r') as sha_file:
-        read_sha = sha_file.read().strip()
+    print(f'Comparing commit shas {sha} and {read_sha}')
 
     if read_sha != sha:
+        print('Updating...')
+
         with open('sha', 'w') as sha_file:
             sha_file.write(sha)
 
@@ -25,9 +29,13 @@ def entry():
         else:
             os.system('git fetch --all || (cd .. ; rm launcher - r ; git clone https://github.com/DragonFIghter603/Launcher.git ; cd Launcher)')
             os.system('git reset --hard origin/main')
-
+        print('Launching update script...')
         os.system('start python cp_entry.py')
+        print('Exiting')
         exit(0)
+
+    print('Continuing with main program...')
+    print()
 
 
 if __name__ == '__main__':
