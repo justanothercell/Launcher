5e685c77a1801487b68c7b0e7cab97ec50a812eb
DragonFighter603 <kuhn.christopher.a@gmail.com>
2022-07-09 11:46:59 +0200

    Update hashpath.py

--- hashpath.py
+++ hashpath.py
@@ -8,24 +8,24 @@ def path_checksum(paths):
     def _update_checksum(checksum, dname, filenames):
         for filename in sorted(filenames):
             path = path_join(dname, filename)
-            checksum.update(path.encode('utf-8'))
-            print(path)
-            fh = open(path, 'rb')
-            while 1:
-                buf = fh.read(4096)
-                if not buf: break
-                checksum.update(buf)
-            fh.close()
+            if isfile(path):
+                checksum.update(path.encode('utf-8'))
+                print(path)
+                fh = open(path, 'rb')
+                while 1:
+                    buf = fh.read(4096)
+                    if not buf: break
+                    checksum.update(buf)
+                fh.close()
 
     chksum = hashlib.sha1()
 
     for path in sorted([normpath(os.getcwd() + '/' + f) for f in paths]):
-        print('p', path)
         if path_exists(path):
             if isdir(path):
                 for bdir, dirs, files in walk(path):
                     _update_checksum(chksum, bdir, files)
             elif isfile(path):
-                _update_checksum(chksum, dirname(path), basename(path))
+                _update_checksum(chksum, dirname(path), [basename(path)])
 
     return chksum.hexdigest()
